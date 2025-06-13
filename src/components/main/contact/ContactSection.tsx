58d0b95e49eebc28a8fa7bb2feea28883f014389
Luiz Silva <luizsv.dev@gmail.com>
2025-06-13 17:21:31 -0300

    Add contact section component

---
+++ src/components/main/contact/ContactSection.tsx
@@ -0,0 +1,18 @@
+import LsContainer from '@/components/ui/LsContainer';
+import { useTranslations } from 'next-intl';
+import React from 'react';
+
+export default function ContactSection(): React.JSX.Element {
+	const intl = useTranslations('appMain.contact');
+
+	return (
+		<LsContainer
+			section="contact"
+			title={intl('title')}
+			coloredBackground
+			forBetterReadability
+		>
+			&nbsp;
+		</LsContainer>
+	);
+}
